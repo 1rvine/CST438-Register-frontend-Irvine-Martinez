36db9a5270348e482a6b70a6fa042c8b242255ce
Irvine <81997674+1rvine@users.noreply.github.com>
2022-11-01 06:09:06 -0700

    added E2E attributes

--- cst438_register_frontend-master/src/components/AddCourse.js
+++ cst438_register_frontend-master/src/components/AddCourse.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
@@ -7,56 +7,71 @@ import DialogTitle from '@mui/material/DialogTitle';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 
-
 // properties addCourse is required, function called when Add clicked.
 class AddCourse extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {open: false, course: {}};
-    };
+	constructor(props) {
+		super(props);
+		this.state = { open: false, course: {} };
+	}
 
-    handleClickOpen = () => {
-        this.setState({open: true});
-    };
+	handleClickOpen = () => {
+		this.setState({ open: true });
+	};
 
-    handleClose = () => {
-        this.setState({open: false});
-    };
+	handleClose = () => {
+		this.setState({ open: false });
+	};
 
-    handleChange = (event) => {
-        this.setState({course: {course_id: event.target.value}});
-    }
+	handleChange = (event) => {
+		this.setState({ course: { course_id: event.target.value } });
+	};
 
-    // Save course and close modal form
-    handleAdd = () => {
-        this.props.addCourse(this.state.course);
-        this.handleClose();
-    }
+	// Save course and close modal form
+	handleAdd = () => {
+		this.props.addCourse(this.state.course);
+		this.handleClose();
+	};
 
-    render() {
-        return (
-            <div>
-                <Button variant="outlined" color="primary" style={{margin: 10}} onClick={this.handleClickOpen}>
-                    Add Course
-                </Button>
-                <Dialog open={this.state.open} onClose={this.handleClose}>
-                    <DialogTitle>Add Course</DialogTitle>
-                    <DialogContent style={{paddingTop: 20}}>
-                        <TextField autoFocus fullWidth label="Course Id" name="course_id" onChange={this.handleChange}/>
-                    </DialogContent>
-                    <DialogActions>
-                        <Button color="secondary" onClick={this.handleClose}>Cancel</Button>
-                        <Button id="Add" color="primary" onClick={this.handleAdd}>Add</Button>
-                    </DialogActions>
-                </Dialog>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div>
+				<Button
+					id={'begin_add_course'}
+					variant="outlined"
+					color="primary"
+					style={{ margin: 10 }}
+					onClick={this.handleClickOpen}
+				>
+					Add Course
+				</Button>
+				<Dialog open={this.state.open} onClose={this.handleClose}>
+					<DialogTitle>Add Course</DialogTitle>
+					<DialogContent style={{ paddingTop: 20 }}>
+						<TextField
+							autoFocus
+							fullWidth
+							label="Course Id"
+							name="course_id"
+							onChange={this.handleChange}
+						/>
+					</DialogContent>
+					<DialogActions>
+						<Button color="secondary" onClick={this.handleClose}>
+							Cancel
+						</Button>
+						<Button id="Add" color="primary" onClick={this.handleAdd}>
+							Add
+						</Button>
+					</DialogActions>
+				</Dialog>
+			</div>
+		);
+	}
 }
 
 // required property:  addCourse is a function to call to perform the Add action
 AddCourse.propTypes = {
-    addCourse: PropTypes.func.isRequired
-}
+	addCourse: PropTypes.func.isRequired,
+};
 
 export default AddCourse;
